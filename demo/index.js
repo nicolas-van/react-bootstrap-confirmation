d631dfedee738983290f94fd6b810a02095062e3
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2019-10-30 13:00:42 +0100

    lint fix

--- demo/index.js
+++ demo/index.js
@@ -37,7 +37,7 @@ const ExampleConfirmModal = () => {
       {result !== undefined ? <p>Result: {JSON.stringify(result)}</p> : undefined}
     </div>
   </div>;
-}
+};
 
 ReactDOM.render(
     <Example/>,
