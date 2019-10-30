6cc1a76c8ece4901e332b8336d91560ed7f0f398
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2019-10-30 10:30:20 +0100

    basic setup

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+  },
+  'extends': [
+    'google',
+    "plugin:react/recommended",
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+  ],
+  "settings": {
+    "react": {
+      "version": "detect",
+    },
+  },
+  'rules': {
+    'max-len': ["error", { "code": 120 }],
+  },
+};
---
+++ demo/index.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+ReactDOM.render(
+    <h1>Hello World</h1>,
+    document.querySelector('#app'),
+);
