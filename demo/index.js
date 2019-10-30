ec6d78debdc69609003a7018276f0f4eba061cf2
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2019-10-30 10:51:39 +0100

    Added working example

--- demo/index.js
+++ demo/index.js
@@ -2,8 +2,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import confirm from '../src';
 
 ReactDOM.render(
-    <h1>Hello World</h1>,
+    <ExampleConfirmModal/>,
     document.querySelector('#app'),
 );
+
+/**
+ * Do stuff
+ * @param {*} confirmButtonStyle the style
+ */
+function displayModal(confirmButtonStyle) {
+  confirm('Are you sure ?', {
+    title: 'Confirm',
+    confirmText: 'Delete',
+    closeText: 'Cancel',
+    confirmButtonStyle,
+  }).then(
+      () => {
+        alert('onConfirm callback');
+      },
+      () => {
+        alert('onCancel callback');
+      },
+  );
+}
+
+/**
+ * Example
+ * @return {*} stuff
+ */
+function ExampleConfirmModal() {
+  return (
+    <div className="container">
+      <h1>Examples</h1>
+      <p>
+        <button type="button" className="btn btn-primary" onClick={() => displayModal('danger')}>
+          Display Confirm Modal red style
+        </button>
+      </p>
+      <p>
+        <button type="button" className="btn btn-primary" onClick={() => displayModal('primary')}>
+          Display Confirm Modal blue style
+        </button>
+      </p>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,82 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {Modal, Button} from 'react-bootstrap';
+import {confirmable, createConfirmation} from 'react-confirm';
+
+/**
+ * A base confirmation class.
+ */
+class Confirmation extends Component {
+  /**
+   * Handles key down.
+   * @param {*} event The event.
+   */
+  handleKeyDown(event) {
+    if (event.target.tagName !== 'BUTTON' && event.keyCode === 13) {
+      this.props.proceed();
+    }
+  }
+
+  /**
+   * @override
+   */
+  render() {
+    return (
+      <Modal
+        size="small"
+        show={this.props.show}
+        onHide={this.props.cancel}
+        backdrop="static"
+        keyboard={this.props.enableEscape}
+        onKeyDown={(event) => this.handleKeyDown(event)}
+      >
+        <Modal.Header>
+          <Modal.Title>{this.props.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{this.props.confirmation}</Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.props.cancel}>
+            {this.props.closeText}
+          </Button>
+          <Button
+            variant={this.props.confirmButtonStyle}
+            onClick={this.props.proceed}
+          >
+            {this.props.confirmText}
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+
+Confirmation.propTypes = {
+  /** header title */
+  title: PropTypes.string,
+  confirmation: PropTypes.string, // arguments of your confirm function
+  confirmText: PropTypes.string,
+  closeText: PropTypes.string,
+  confirmButtonStyle: PropTypes.oneOf(['primary', 'success', 'danger', 'warning', 'info', 'default']),
+  show: PropTypes.bool, // from confirmable. indicates if the dialog is shown or not.
+  proceed: PropTypes.func, // from confirmable. call to close the dialog with promise resolved.
+  cancel: PropTypes.func, // from confirmable. call to close the dialog with promise rejected.
+  enableEscape: PropTypes.bool,
+};
+
+Confirmation.defaultProps = {
+  title: 'Confirm',
+  confirmation: undefined,
+  confirmText: 'OK',
+  closeText: 'Cancel',
+  confirmButtonStyle: 'primary',
+  show: undefined,
+  proceed: undefined,
+  cancel: undefined,
+  enableEscape: true,
+};
+
+const confirm = createConfirmation(confirmable(Confirmation));
+
+export default (confirmation, options = {}) => {
+  return confirm(Object.assign({confirmation}, options));
+};
